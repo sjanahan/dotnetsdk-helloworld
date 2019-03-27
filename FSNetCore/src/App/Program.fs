1d8015bdded54945fbf6fda0a44b33d9519ece9f
Jay Sivaraman <jay.sivaraman@jet.com>
2019-03-27 17:21:53 -0400

    Getting started with F# with the .NET Core CLI

---
+++ FSNetCore/src/App/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open Library
+
+[<EntryPoint>]
+let main argv =
+    printfn "Nice command-line arguments! Here's what JSON.NET has to say about them:"
+
+    argv
+    |> Array.map getJsonNetJson
+    |> Array.iter (printfn "%s")
+
+    0 // return an integer exit code
